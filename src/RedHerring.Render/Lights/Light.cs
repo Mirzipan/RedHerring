faf91fee3c063a4840983bdb72c9b0606b387421
Mirzipan <the.last.judge@gmail.com>
2023-10-01 22:09:59 +0200

    🚧light component + render-side light

---
+++ src/RedHerring.Render/Lights/Light.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using Vortice.Mathematics;
+
+namespace RedHerring.Render.Lights;
+
+public class Light
+{
+    public Matrix4x4 WorldMatrix;
+    public float Intensity;
+    public Vector3 Position;
+    public Vector3 Direction;
+    public Color3 Color;
+    public BoundingBox BoundingBox;
+}
