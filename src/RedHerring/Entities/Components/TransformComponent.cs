42b67fa60c02ce37659cecc7fe07d8e428a83f49
Mirzipan <the.last.judge@gmail.com>
2023-10-05 23:23:40 +0200

    🚧transform component now contains parent and children

--- src/RedHerring/Entities/Components/TransformComponent.cs
+++ src/RedHerring/Entities/Components/TransformComponent.cs
@@ -4,8 +4,19 @@ namespace RedHerring.Entities.Components;
 
 public sealed class TransformComponent : AnEntityComponent
 {
-    private Matrix4x4 _worldMatrix = Matrix4x4.Identity;
-    private Matrix4x4 _localMatrix = Matrix4x4.Identity;
+    private TransformComponent? _parent;
+    private TransformChildrenCollection _children;
+    
+    private Matrix4x4 _worldMatrix;
+    private Matrix4x4 _localMatrix;
+
+    public TransformComponent? Parent
+    {
+        get => _parent;
+        set => _parent = value; // TODO: this might need extra update stuff
+    }
+
+    public TransformChildrenCollection Children => _children;
 
     public Matrix4x4 WorldMatrix
     {
@@ -17,4 +28,20 @@ public sealed class TransformComponent : AnEntityComponent
         get => _localMatrix;
         set => _localMatrix = value;
     }
+
+    public Vector3 Right => new Vector3(_worldMatrix.M11, _worldMatrix.M21, _worldMatrix.M31);
+    public Vector3 Up => new Vector3(_worldMatrix.M12, _worldMatrix.M22, _worldMatrix.M32);
+    public Vector3 Forward => new Vector3(_worldMatrix.M13, _worldMatrix.M23, _worldMatrix.M33);
+
+    #region Lifecycle
+
+    public TransformComponent()
+    {
+        _children = new TransformChildrenCollection(this);
+        
+        _worldMatrix = Matrix4x4.Identity;
+        _localMatrix = Matrix4x4.Identity;
+    }
+
+    #endregion Lifecycle
 }
