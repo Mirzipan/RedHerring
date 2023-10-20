c8b10ea8659ad8ebcf0916d57d1e31b80d490975
Mirzipan <the.last.judge@gmail.com>
2023-10-20 21:03:32 +0200

    ✨struct to store indices describing a region of an array

---
+++ src/RedHerring.Numbers/Region.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RedHerring.Numbers;
+
+[StructLayout(LayoutKind.Sequential)]
+public record struct Region(int Start, int Length)
+{
+    public int Start = Start;
+    public int Length = Length;
+
+    public bool IsEmpty => Length == 0;
+    public bool IsValid => Start >= 0 && Length > 0;
+
+    public int Next => Start + Length;
+    public int End => Start + Length - 1;
+}
