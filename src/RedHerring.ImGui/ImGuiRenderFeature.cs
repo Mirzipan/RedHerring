725f77c01398c2585fb10295b27e8de33cf927ab
Mirzipan <the.last.judge@gmail.com>
2023-10-22 16:54:53 +0200

    ♻️imgui reworked to be a render feature, rather than trying to draw itself from the system

---
+++ src/RedHerring.ImGui/ImGuiRenderFeature.cs
@@ -0,0 +1,134 @@
+﻿using System.Numerics;
+using ImGuiNET;
+using RedHerring.Alexandria;
+using RedHerring.Render.Features;
+using RedHerring.Render.Passes;
+using Silk.NET.Maths;
+using Veldrid;
+using Vortice.Mathematics;
+using Gui = ImGuiNET.ImGui;
+
+namespace RedHerring.ImGui;
+
+public class ImGuiRenderFeature : ARenderFeature
+{
+    private const ImGuiWindowFlags BackgroundWindowFlags = ImGuiWindowFlags.NoSavedSettings 
+                                                           | ImGuiWindowFlags.NoCollapse 
+                                                           | ImGuiWindowFlags.NoTitleBar 
+                                                           | ImGuiWindowFlags.NoResize 
+                                                           | ImGuiWindowFlags.NoScrollbar 
+                                                           | ImGuiWindowFlags.NoMove 
+                                                           | ImGuiWindowFlags.NoBackground;
+    
+    private ImGuiRenderer? _renderer;
+    private byte[] _openSansData = null!;
+    private ImFontConfigPtr _openSansConfig;
+
+    public override int Priority { get; } = -1_000_000;
+    public Vector2D<int> Size { get; set; }
+
+    #region Lifecycle
+
+    public override void Init(GraphicsDevice device, CommandList commandList)
+    {
+        LoadDefaultFontData();
+        ResetRenderer(device);
+    }
+
+    public override void Update(GraphicsDevice device, CommandList commandList)
+    {
+    }
+
+    public override void Render(GraphicsDevice device, CommandList commandList, RenderPass pass)
+    {        
+        Gui.SetNextWindowPos(Vector2.Zero);
+        Gui.SetNextWindowSize((Vector2)Size);
+        Gui.Begin("Canvas", BackgroundWindowFlags);
+        
+        Gui.TextColored(Color3.AliceBlue.ToVector4(), "Debug");
+        DrawDeviceInfo(device);
+        Gui.End();
+        
+        _renderer?.Render(device, commandList);
+    }
+
+    public override void Resize(Vector2D<int> size)
+    {
+        Size = size;
+        _renderer?.WindowResized(size.X, size.Y);
+    }
+
+    public override void Destroy()
+    {
+        FontLoader.Unload();
+
+        if (_renderer is not null)
+        {
+            _renderer.ClearCachedImageResources();
+            _renderer.DestroyDeviceObjects();
+            _renderer.Dispose();
+        }
+        
+        FontLoader.Unloaded(_openSansConfig);
+    }
+
+    #endregion Lifecycle
+
+    #region Public
+
+    public void Update(GameTime time, InputSnapshot snapshot)
+    {
+        _renderer?.Update(time.Elapsed, snapshot);
+    }
+
+    #endregion Public
+
+    #region Private
+
+    private void ResetRenderer(GraphicsDevice device)
+    {
+        if (_renderer is not null)
+        {
+            _renderer.ClearCachedImageResources();
+            _renderer.DestroyDeviceObjects();
+            _renderer.Dispose();
+        }
+
+
+        var size = Size;
+        _renderer = new ImGuiRenderer(
+            device,
+            device.MainSwapchain.Framebuffer.OutputDescription,
+            size.X,
+            size.Y);
+
+        RecreateFont();
+    }
+    
+    private void LoadDefaultFontData()
+    {
+        _openSansConfig = FontLoader.LoadFontData(Base64Font.OpenSans, out _openSansData);
+    }
+
+    private void RecreateFont()
+    {
+        FontLoader.RecreateFont(_renderer!, _openSansData, _openSansConfig);
+    }
+
+    private void DrawDeviceInfo(GraphicsDevice device)
+    {
+        Gui.Text("Device Info");
+        AddRow("Name:", device.DeviceName);
+        AddRow("Backend:", device.BackendType.ToString());
+        AddRow("API Version:", device.ApiVersion.ToString()!);
+    }
+
+    private static void AddRow(string column1, string column2)
+    {
+        Gui.TextColored(Color3.LightGray.ToVector4(), column1);
+        Gui.SameLine();
+        Gui.Text(column2);
+    }
+
+    #endregion Private
+}
--- src/RedHerring.ImGui/ImGuiSystem.cs
+++ src/RedHerring.ImGui/ImGuiSystem.cs
@@ -1,35 +1,20 @@
-﻿using System.Numerics;
-using ImGuiNET;
-using RedHerring.Alexandria;
+﻿using RedHerring.Alexandria;
 using RedHerring.Core;
 using RedHerring.Core.Systems;
 using RedHerring.Infusion.Attributes;
-using Veldrid;
-using Vortice.Mathematics;
 using Gui = ImGuiNET.ImGui;
 
 namespace RedHerring.ImGui;
 
 public class ImGuiSystem : AnEngineSystem, IDrawable
 {
-    private const ImGuiWindowFlags BackgroundWindowFlags = ImGuiWindowFlags.NoSavedSettings 
-                                                 | ImGuiWindowFlags.NoCollapse 
-                                                 | ImGuiWindowFlags.NoTitleBar 
-                                                 | ImGuiWindowFlags.NoResize 
-                                                 | ImGuiWindowFlags.NoScrollbar 
-                                                 | ImGuiWindowFlags.NoMove 
-                                                 | ImGuiWindowFlags.NoBackground;
-    
     [Inject]
     private InputSystem _inputSystem = null!;
     [Inject]
     private GraphicsSystem _graphicsSystem = null!;
 
-    private RedInputSnapshot _inputSnapshot = null!;
-    
-    private ImGuiRenderer? _renderer;
-    private byte[] _openSansData = null!;
-    private ImFontConfigPtr _openSansConfig;
+    private GuiInputSnapshot _inputSnapshot = null!;
+    private ImGuiRenderFeature _feature = null!;
     
     public bool IsVisible => true;
     public int DrawOrder => 10_000_000;
@@ -42,24 +27,13 @@ public class ImGuiSystem : AnEngineSystem, IDrawable
 
     protected override void Load()
     {
-        _inputSnapshot = new RedInputSnapshot(_inputSystem.Input);
-
-        LoadDefaultFontData();
-        ResetRenderer();
+        _inputSnapshot = new GuiInputSnapshot(_inputSystem.Input);
+        _feature = new ImGuiRenderFeature();
+        _graphicsSystem.RegisterFeature(_feature);
     }
 
     protected override void Unload()
     {
-        FontLoader.Unload();
-
-        if (_renderer is not null)
-        {
-            _renderer.ClearCachedImageResources();
-            _renderer.DestroyDeviceObjects();
-            _renderer.Dispose();
-        }
-        
-        FontLoader.Unloaded(_openSansConfig);
     }
 
     #endregion Lifecycle
@@ -74,76 +48,12 @@ public class ImGuiSystem : AnEngineSystem, IDrawable
     public void Draw(GameTime gameTime)
     {
         _inputSnapshot.Update();
-        _renderer?.Update(gameTime.Elapsed, _inputSnapshot);
-        
-        Gui.SetNextWindowPos(Vector2.Zero);
-        Gui.SetNextWindowSize((Vector2)Context.View.Size);
-        Gui.Begin("Canvas", BackgroundWindowFlags);
-
-        Gui.TextColored(Color3.AliceBlue.ToVector4(), "Debug");
-        DrawDeviceInfo();
-        
-        Gui.End();
+        _feature.Update(gameTime, _inputSnapshot);
     }
 
     public void EndDraw()
     {
-        var device = _graphicsSystem.Device;
-        var cl = _graphicsSystem.CommandList;
-        
-        _renderer?.Render(device, cl);
     }
 
     #endregion Drawable
-
-    #region Private
-
-    private void ResetRenderer()
-    {
-        if (_renderer is not null)
-        {
-            _renderer.ClearCachedImageResources();
-            _renderer.DestroyDeviceObjects();
-            _renderer.Dispose();
-        }
-
-
-        var size = Context.View.FramebufferSize;
-        _renderer = new ImGuiRenderer(
-            _graphicsSystem.Device,
-            _graphicsSystem.Device.MainSwapchain.Framebuffer.OutputDescription,
-            size.X,
-            size.Y);
-
-        RecreateFont();
-    }
-    
-    private void LoadDefaultFontData()
-    {
-        _openSansConfig = FontLoader.LoadFontData(Base64Font.OpenSans, out _openSansData);
-    }
-
-    private void RecreateFont()
-    {
-        FontLoader.RecreateFont(_renderer!, _openSansData, _openSansConfig);
-    }
-
-    private void DrawDeviceInfo()
-    {
-        var device = _graphicsSystem.Device;
-        
-        Gui.Text("Device Info");
-        AddRow("Name:", device.DeviceName);
-        AddRow("Backend:", device.BackendType.ToString());
-        AddRow("API Version:", device.ApiVersion.ToString()!);
-    }
-
-    private static void AddRow(string column1, string column2)
-    {
-        Gui.TextColored(Color3.LightGray.ToVector4(), column1);
-        Gui.SameLine();
-        Gui.Text(column2);
-    }
-
-    #endregion Private
 }
