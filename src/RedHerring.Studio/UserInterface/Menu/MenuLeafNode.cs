f4b90700e4ffd3bcd843d419e0648cff2644efa4
Miroslav Kovac <assembler_bot@yahoo.com>
2023-10-25 19:10:02 +0200

    ✨added checked and disabled menu state functions

--- src/RedHerring.Studio/UserInterface/Menu/MenuLeafNode.cs
+++ src/RedHerring.Studio/UserInterface/Menu/MenuLeafNode.cs
@@ -11,7 +11,7 @@ public sealed class MenuLeafNode : AMenuNode
 
 	public override void Update()
 	{
-		if (Gui.MenuItem(Name))
+		if (Gui.MenuItem(Name, _shortcut, _isChecked?.Invoke() ?? false, _isEnabled?.Invoke() ?? true))
 		{
 			_onClick.Invoke();
 		}
