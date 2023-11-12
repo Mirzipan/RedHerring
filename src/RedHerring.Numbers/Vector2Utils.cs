dc36fd7be6420128f81cde6d8310b8e8415c2f84
Mirzipan <the.last.judge@gmail.com>
2023-11-12 19:30:59 +0100

    ♻️vector utils now use properties instead of readonly fields

--- src/RedHerring.Numbers/Vector2Utils.cs
+++ src/RedHerring.Numbers/Vector2Utils.cs
@@ -1,17 +1,46 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace RedHerring.Numbers;
 
 public static class Vector2Utils
 {
-    public static readonly Vector2 NegativeOne = new Vector2(-1f);
-    public static readonly Vector2 Half = new Vector2(-0.5f);
+    public static Vector2 NegativeOne 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector2(-1f);
+    }
     
-    public static readonly Vector2 Up = new Vector2(0, 1);
-    public static readonly Vector2 Down = new Vector2(0, -1);
+    public static Vector2 Half 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector2(-0.5f);
+    }
     
-    public static readonly Vector2 Left = new Vector2(-1, 0);
-    public static readonly Vector2 Right = new Vector2(1, 0);
+    public static Vector2 Up 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector2(0, 1);
+    }
+    
+    public static Vector2 Down 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector2(0, -1);
+    }
+    
+    public static Vector2 Left 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector2(-1, 0);
+    }
+    
+    public static Vector2 Right 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector2(1, 0);
+    }
+    
 
 
 }
--- src/RedHerring.Numbers/Vector3Utils.cs
+++ src/RedHerring.Numbers/Vector3Utils.cs
@@ -1,18 +1,55 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace RedHerring.Numbers;
 
 public static class Vector3Utils
 {
-    public static readonly Vector3 NegativeOne = new Vector3(-1f);
-    public static readonly Vector3 Half = new Vector3(-0.5f);
-    
-    public static readonly Vector3 Up = new Vector3(0, 1, 0);
-    public static readonly Vector3 Down = new Vector3(0, -1, 0);
-    
-    public static readonly Vector3 Left = new Vector3(-1, 0, 0);
-    public static readonly Vector3 Right = new Vector3(1, 0, 0);
-    
-    public static readonly Vector3 Forward = new Vector3(0, 0, -1);
-    public static readonly Vector3 Backward = new Vector3(0, 0, 1);
+    public static Vector3 NegativeOne
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(-1f);
+    }
+
+    public static Vector3 Half
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(-0.5f);
+    }
+
+    public static Vector3 Up
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(0, 1, 0);
+    }
+
+    public static Vector3 Down
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(0, -1, 0);
+    }
+
+    public static Vector3 Left
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(-1, 0, 0);
+    }
+
+    public static Vector3 Right
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(1, 0, 0);
+    }
+
+    public static Vector3 Forward
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(0, 0, -1);
+    }
+
+    public static Vector3 Backward
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => new Vector3(0, 0, 1);
+    }
 }
