0899c4417ee060c29e4296291c6e716fe4ca7c03
Miroslav Kovac <assembler_bot@yahoo.com>
2023-11-14 22:04:13 +0100

    ✨list control empty class

---
+++ src/RedHerring.Studio/UserInterface/Inspector/Controls/ComplexControls/InspectorListControl.cs
@@ -0,0 +1,14 @@
+﻿namespace RedHerring.Studio.UserInterface;
+
+public sealed class InspectorListControl : AnInspectorControl
+{
+	public InspectorListControl(Inspector inspector, string id) : base(inspector, id)
+	{
+		
+	}
+
+	public override void Update()
+	{
+		
+	}
+}
