3ce26ca5356671db733bcd6baa1dff87ad3d7df7
Mirzipan <the.last.judge@gmail.com>
2023-11-17 17:10:08 +0100

    ⚗️added matrices to debug shader

--- src/RedHerring.Render/ShaderFactory.cs
+++ src/RedHerring.Render/ShaderFactory.cs
@@ -9,6 +9,12 @@ public static class ShaderFactory
     private const string VertexCode = @"
 #version 450
 
+layout(binding = 0) uniform MatrixBlock
+{
+    mat4 ProjectionMatrix;
+    mat4 ViewMatrix;
+} matrices;
+
 layout(location = 0) in vec2 Position;
 layout(location = 1) in vec4 Color;
 
@@ -16,7 +22,8 @@ layout(location = 0) out vec4 fsin_Color;
 
 void main()
 {
-    gl_Position = vec4(Position, 0, 1);
+    mat4 vp = matrices.ProjectionMatrix * matrices.ViewMatrix;
+    gl_Position = vp * vec4(Position, 0, 1);
     fsin_Color = Color;
 }";
 
