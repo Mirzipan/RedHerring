f64da68ed7191ee66ee9963a314c4d676fa4ab24
Mirzipan <the.last.judge@gmail.com>
2023-11-19 19:52:29 +0100

    🚧HLSL debug shaders

---
+++ src/RedHerring.Render/Shaders/debug_vertexcolor_fragment.hlsl
@@ -0,0 +1,11 @@
+﻿struct PixelInput
+{
+    float4 position : POSITION;
+    float4 color : COLOR;
+};
+
+float4 PS(PixelInput input) : SV_TARGET
+{
+    return input.color;
+}
+
---
+++ src/RedHerring.Render/Shaders/debug_vertexcolor_vertex.hlsl
@@ -0,0 +1,40 @@
+﻿cbuffer ProjectionBuffer : register(b0)
+{
+    float4x4 projection;
+}
+
+cbuffer ViewBuffer : register(b1)
+{
+    float4x4 view;
+}
+
+cbuffer WorldBuffer : register(b2)
+{
+    float4x4 world;
+}
+
+struct VertexInput
+{
+    float3 position : POSITION;
+    float4 color : COLOR;
+};
+
+struct VertexOutput
+{
+    float4 position : POSITION;
+    float4 color : COLOR;
+};
+
+VertexOutput VS(VertexInput input)
+{    
+    float4 worldPosition = mul(world, float4(input.position, 1));
+    float4 viewPosition = mul(view, worldPosition);
+    float4 clipPosition = mul(projection, viewPosition);
+    
+    VertexOutput output;
+    output.position = clipPosition;    
+    output.color = input.color;
+    
+    return output;
+}
+
