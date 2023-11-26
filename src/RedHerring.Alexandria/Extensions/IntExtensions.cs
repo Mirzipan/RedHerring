934b28616789f04b949d8ed0d37d7ebab369bdea
Mirzipan <the.last.judge@gmail.com>
2023-11-26 16:04:39 +0100

    🎨int extensions formatting

--- src/RedHerring.Alexandria/Extensions/IntExtensions.cs
+++ src/RedHerring.Alexandria/Extensions/IntExtensions.cs
@@ -4,40 +4,40 @@ namespace RedHerring.Alexandria.Extensions;
 
 public static partial class IntExtensions
 {
-        #region Equality
+    #region Equality
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsZero(this int @this) => @this == 0;
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool NotZero(this int @this) => @this != 0;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsZero(this int @this) => @this == 0;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsMin(this int @this) => @this == int.MinValue;
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsMax(this int @this) => @this == int.MaxValue;
-        
-        #endregion Equality
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool NotZero(this int @this) => @this != 0;
 
-        #region Manipulation
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsMin(this int @this) => @this == int.MinValue;
 
-        /// <summary>
-        /// Clamps this between min and max values.
-        /// </summary>
-        /// <param name="this"></param>
-        /// <param name="min">Lower bound</param>
-        /// <param name="max">Upped bound</param>
-        /// <returns></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Clamp(this int @this, int min, int max) => Math.Clamp(@this, min, max);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsMax(this int @this) => @this == int.MaxValue;
 
-        /// <summary>
-        /// Returns -1 for negative numbers, 1 for positive numbers and 0 for zero.
-        /// </summary>
-        /// <param name="this"></param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Sign(this int @this) => @this == 0 ? 0 : (@this > 0 ? 1 : -1);
-        
-        #endregion Manipulation
+    #endregion Equality
+
+    #region Manipulation
+
+    /// <summary>
+    /// Clamps this between min and max values.
+    /// </summary>
+    /// <param name="this"></param>
+    /// <param name="min">Lower bound</param>
+    /// <param name="max">Upped bound</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Clamp(this int @this, int min, int max) => Math.Clamp(@this, min, max);
+
+    /// <summary>
+    /// Returns -1 for negative numbers, 1 for positive numbers and 0 for zero.
+    /// </summary>
+    /// <param name="this"></param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Sign(this int @this) => @this == 0 ? 0 : (@this > 0 ? 1 : -1);
+
+    #endregion Manipulation
 }
