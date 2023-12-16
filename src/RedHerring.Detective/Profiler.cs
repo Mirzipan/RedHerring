7d646cde63c84ff0f14c9ef9e319498752219144
Mirzipan <the.last.judge@gmail.com>
2023-12-16 20:27:23 +0100

    💡profiler - todos

--- src/RedHerring.Detective/Profiler.cs
+++ src/RedHerring.Detective/Profiler.cs
@@ -2,6 +2,20 @@
 
 public static class Profiler
 {
+    private static Task _profilerTask;
+    
+    
+    // TODO(mirzipan) expose recorded data
+    // TODO(mirzipan) figure out how long to keep data for
+    
+    static Profiler()
+    {
+        _profilerTask = Task.Run(async () =>
+        {
+            // TODO(mirzipan) await stuff
+        });
+    }
+    
     public static IDisposable Begin(string name)
     {
         var key = new ProfilingKey(name);
