3f7ce8b7623023b5e3889a86a6c39f1fa29af075
Mirzipan <the.last.judge@gmail.com>
2023-12-17 18:42:24 +0100

    🔥unused extension

--- src/RedHerring.Render/ImGui/ColorExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Numerics;
-using Vortice.Mathematics;
-
-namespace RedHerring.Render.ImGui;
-
-public static class ColorExtensions
-{
-    public static Vector4 ToVector4(this Color3 @this) => new(@this.R, @this.G, @this.B, 1f);
-}
