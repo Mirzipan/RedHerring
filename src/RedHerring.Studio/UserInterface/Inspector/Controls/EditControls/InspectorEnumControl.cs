be579ac900f1b4739cce1b4d1ed6bd4f0c98e821
Miroslav Kovac <assembler_bot@yahoo.com>
2024-01-11 16:00:16 +0100

    🐛enum controls now handles out of order values

--- src/RedHerring.Studio/UserInterface/Inspector/Controls/EditControls/InspectorEnumControl.cs
+++ src/RedHerring.Studio/UserInterface/Inspector/Controls/EditControls/InspectorEnumControl.cs
@@ -5,7 +5,9 @@ namespace RedHerring.Studio.UserInterface;
 
 public class InspectorEnumControl : InspectorSingleInputControl<Enum>
 {
-	private string[] _items = null!;
+	private string[]            _items       = null!;
+	private Dictionary<int,int> _enumToIndex = null!;
+	private int[]               _indexToEnum = null!;
 	
 	public InspectorEnumControl(IInspectorCommandTarget commandTarget, string id) : base(commandTarget, id)
 	{
@@ -15,14 +17,25 @@ public class InspectorEnumControl : InspectorSingleInputControl<Enum>
 	{
 		base.InitFromSource(sourceOwner, source, sourceField, sourceIndex);
 		_items = Enum.GetNames(Value!.GetType());
+
+		_enumToIndex = new Dictionary<int,int>();
+		_indexToEnum = new int[_items.Length];
+
+		int index = 0;
+		foreach (object? value in Enum.GetValues(Value!.GetType()))
+		{
+			int enumValue = (int) value;
+			_enumToIndex[enumValue] = index;
+			_indexToEnum[index]     = enumValue;
+			++index;
+		}
 	}
 	
 	protected override bool InputControl(bool makeItemActive)
 	{
-		int  localValue = Convert.ToInt32(Value!);
+		int  localValue = _enumToIndex[Convert.ToInt32(Value!)];
 		bool submit     = Gui.Combo(LabelId, ref localValue, _items, _items.Length);
-		Value = (Enum?)Enum.ToObject(Value!.GetType(), localValue);
-		
+		Value = (Enum?)Enum.ToObject(Value!.GetType(), _indexToEnum[localValue]);
 		return submit || makeItemActive;
 	}
 }
