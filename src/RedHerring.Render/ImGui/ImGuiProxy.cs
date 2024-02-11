04df9012d9c644c747449a7006928e503841f9c0
Mirzipan <the.last.judge@gmail.com>
2024-02-11 15:43:56 +0100

    ✨ImGuiProxy - binding creation and removal

--- src/RedHerring.Render/ImGui/ImGuiProxy.cs
+++ src/RedHerring.Render/ImGui/ImGuiProxy.cs
@@ -8,10 +8,40 @@ namespace RedHerring.Render.ImGui;
 public static class ImGuiProxy
 {
     private static ImGuiRenderer? _renderer;
+    private static GraphicsDevice _device;
+    private static ResourceFactory _factory;
+
+    public static void SetDevice(GraphicsDevice device)
+    {
+        _device = device;
+        _factory = device.ResourceFactory;
+    }
     
     public static void Update(GameTime time, Input input)
     {
         _renderer?.Update((float)time.Elapsed.TotalSeconds, input);
+    }
+
+    public static IntPtr GetOrCreateImGuiBinding(TextureView textureView)
+    {
+        IntPtr? result = _renderer?.GetOrCreateImGuiBinding(_factory, textureView);
+        return result ?? IntPtr.Zero;
+    }
+
+    public static void RemoveImGuiBinding(TextureView textureView)
+    {
+        _renderer?.RemoveImGuiBinding(textureView);
+    }
+
+    public static IntPtr GetOrCreateImGuiBinding(Texture texture)
+    {
+        IntPtr? result = _renderer?.GetOrCreateImGuiBinding(_factory, texture);
+        return result ?? IntPtr.Zero;
+    }
+
+    public static void RemoveImGuiBinding(Texture texture)
+    {
+        _renderer?.RemoveImGuiBinding(texture);
     }
     
     internal static void ResetImGuiRenderer(ref ImGuiRenderer? renderer, GraphicsDevice device, int width, int height)
---
+++ src/RedHerring.Studio/UserInterface/Editor/ImageFile.cs
@@ -0,0 +1,13 @@
+﻿using RedHerring.Render.ImGui;
+using Veldrid;
+using Gui = ImGuiNET.ImGui;
+
+namespace RedHerring.Studio.UserInterface.Editor;
+
+public static class ImageFile
+{
+    public static void Draw(string path)
+    {
+        //ImGuiProxy.GetOrCreateImGuiBinding();
+    }
+}
