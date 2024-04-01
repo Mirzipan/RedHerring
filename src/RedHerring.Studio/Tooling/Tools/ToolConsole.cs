7c9feb2487d422fcb5e54a50529336ba887cd3d2
Mirzipan <the.last.judge@gmail.com>
2024-04-01 15:52:42 +0200

    🔨studio - console now includes copy to clipboard on right-click

--- src/RedHerring.Studio/Tooling/Tools/ToolConsole.cs
+++ src/RedHerring.Studio/Tooling/Tools/ToolConsole.cs
@@ -1,7 +1,9 @@
-﻿using ImGuiNET;
+﻿using System.Globalization;
+using ImGuiNET;
 using RedHerring.Render.ImGui;
 using RedHerring.Studio.Models;
 using RedHerring.Studio.Models.ViewModels.Console;
+using RedHerring.Studio.UserInterface;
 using Gui = ImGuiNET.ImGui;
 
 namespace RedHerring.Studio.Tools;
@@ -13,6 +15,9 @@ public sealed class ToolConsole : Tool
     
     protected override string Name => ToolName;
 
+    private Menu _contextMenu = new(MenuStyle.ContextMenu);
+    private string _selectedLine = string.Empty;
+
     private bool _showTimestamps = true;
     private bool _showInfo = true;
     private bool _showSuccess = true;
@@ -22,11 +27,13 @@ public sealed class ToolConsole : Tool
 
     public ToolConsole(StudioModel studioModel, int uniqueId) : base(studioModel, uniqueId)
     {
+        PopulateContextMenu();
     }
 
     public override void Update(out bool finished)
     {
         finished = Draw();
+        _contextMenu.InvokeClickActions();
     }
 
     private bool Draw()
@@ -35,20 +42,8 @@ public sealed class ToolConsole : Tool
         
         if (Gui.Begin(NameId, ref isOpen, ImGuiWindowFlags.HorizontalScrollbar))
         {
-            Gui.BeginGroup();
-            Gui.Checkbox("Timestamps", ref _showTimestamps);
-            Gui.SameLine();
-            Gui.Checkbox("Info", ref _showInfo);
-            Gui.SameLine();
-            Gui.Checkbox("Success", ref _showSuccess);
-            Gui.SameLine();
-            Gui.Checkbox("Warning", ref _showWarning);
-            Gui.SameLine();
-            Gui.Checkbox("Error", ref _showError);
-            Gui.SameLine();
-            Gui.Checkbox("Exception", ref _showException);
-            Gui.EndGroup();
-            
+            DrawSettings();
+
             Gui.SeparatorText("Messages");
             
             for (int i = 0; i < StudioModel.Console.Count; ++i)
@@ -97,13 +92,50 @@ public sealed class ToolConsole : Tool
                     Gui.TextColored(color, item.TimeStamp.ToLongTimeString());
                     Gui.SameLine();
                 }
-                
+
+                string id = item.TimeStamp.ToString(CultureInfo.InvariantCulture);
+                Gui.PushID(id);
                 Gui.TextColored(color, item.Message);
+                Gui.PopID();
+
+                if (Gui.BeginPopupContextItem(id))
+                {
+                    _selectedLine = item.Message;
+                    _contextMenu.Update();
+                    Gui.EndPopup();
+                }
             }
 
             Gui.End();
         }
 
         return !isOpen;
+    }
+
+    private void DrawSettings()
+    {
+        Gui.BeginGroup();
+        Gui.Checkbox("Timestamps", ref _showTimestamps);
+        Gui.SameLine();
+        Gui.Checkbox("Info", ref _showInfo);
+        Gui.SameLine();
+        Gui.Checkbox("Success", ref _showSuccess);
+        Gui.SameLine();
+        Gui.Checkbox("Warning", ref _showWarning);
+        Gui.SameLine();
+        Gui.Checkbox("Error", ref _showError);
+        Gui.SameLine();
+        Gui.Checkbox("Exception", ref _showException);
+        Gui.EndGroup();
+    }
+
+    private void PopulateContextMenu()
+    {
+        _contextMenu.AddItem("Copy", CopySelectedLineToClipboard);
+    }
+
+    private void CopySelectedLineToClipboard()
+    {
+        Gui.SetClipboardText(_selectedLine);
     }
 }
