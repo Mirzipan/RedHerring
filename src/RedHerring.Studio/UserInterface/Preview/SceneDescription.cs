2e593f797f2567df0f4526011f8b5825b22e759b
Mirzipan <the.last.judge@gmail.com>
2024-04-03 13:48:18 +0200

    🚧importer - silk assimp vol.5 - file preview now works with the new importer

--- src/RedHerring.Studio/UserInterface/Preview/SceneDescription.cs
+++ src/RedHerring.Studio/UserInterface/Preview/SceneDescription.cs
@@ -1,28 +1,36 @@
-﻿namespace RedHerring.Studio.UserInterface.Editor;
+﻿using RedHerring.Studio.Import;
+
+namespace RedHerring.Studio.UserInterface.Editor;
 
 internal class SceneDescription
 {
-    private readonly SceneImporterSettings _settings;
-
     private readonly string[] _meshNames;
     private readonly string[] _materialNames;
     private readonly string[] _animationNames;
+    private readonly string _rootName;
 
     public string[] MeshNames => _meshNames;
     public string[] MaterialNames => _materialNames;
     public string[] AnimationNames => _animationNames;
     
-    public int MeshCount => _settings.Meshes.Count;
-    public int MaterialCount => _settings.Materials.Count;
-    public int AnimationCount => _settings.Animations.Count;
-    public string RootName => _settings.Root.Name;
+    public int MeshCount => _meshNames.Length;
+    public int MaterialCount => _materialNames.Length;
+    public int AnimationCount => _animationNames.Length;
+    public string RootName => _rootName;
     
     public SceneDescription(SceneImporterSettings settings)
     {
-        _settings = settings;
-
         _meshNames = settings.Meshes.Select(e => e.Name).ToArray();
         _materialNames = settings.Materials.Select(e => e.Name).ToArray();
         _animationNames = settings.Animations.Select(e => e.Name).ToArray();
+        _rootName = settings.Root.Name;
+    }
+
+    public SceneDescription(AssimpSceneImporterSettings settings)
+    {
+        _meshNames = settings.Meshes.Select(e => e.Name).ToArray();
+        _materialNames = settings.Materials.Select(e => e.Name).ToArray();
+        _animationNames = settings.Animations.Select(e => e.Name).ToArray();
+        _rootName = settings.Root.Name;
     }
 }
